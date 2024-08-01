6bf2e4f96ea5427e37755f02b3239af595575dd7
wins-li <wli219312@gmail.com>
2024-07-31 22:18:36 -0400

    dropdown menu experimentation

--- src/app/callback/page.js
+++ src/app/callback/page.js
@@ -1,9 +1,11 @@
+"use client"
 import Image from "next/image";
 import styles from "/styles/page.module.css";
-
-
+import "@/styles/globals.css";
+import DropDownMenu from "@/components/dropdown.js"
 
 const dashboard = () => {
+  
   return (
     <main className={styles.main}>
       <div className={styles.description}>
@@ -12,10 +14,7 @@ const dashboard = () => {
       <div className={styles.center}>
           <p className={styles.logo}>Rectify</p>
       </div>
-      
-      
-
-      
+      <DropDownMenu/>
     </main>
   );
 }
